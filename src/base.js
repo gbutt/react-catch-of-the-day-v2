f68f017108bae8f4927cdf90a3a9bc3266814816
Conner Simmering <connersimmering@gmail.com>
2017-08-14 09:20:52 -0600

    Safe firebase credentials.

--- src/base.js
+++ src/base.js
@@ -3,9 +3,9 @@ import Rebase from 're-base'
 //Safe to expose, obfuscated anyways.
 //https://stackoverflow.com/questions/37482366/is-it-safe-to-expose-firebase-apikey-to-the-public
 const base = Rebase.createClass({
-    apiKey: process.env.REACT_APP_RCOTDV2apiKey,
-    authDomain: process.env.REACT_APP_RCOTDV2authDomain,
-    databaseURL: process.env.REACT_APP_RCOTDV2databaseURL,
+    apiKey: "AIzaSyDY_tXsW3ksMsKRTZuWiPnbt7l6jsdIzRU",
+    authDomain: "react-catch-of-the-day-v2.firebaseapp.com",
+    databaseURL: "https://react-catch-of-the-day-v2.firebaseio.com",
 });
 
 export default base;
